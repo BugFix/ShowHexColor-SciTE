a20221b871950871ebb46c867c2d289e1322ede0
BugFix <AutoIt@bug-fix.info>
2018-02-20 18:21:11 +0100

    Update ShowHexColorFromCursor.lua
    current version 0.9
    fixed: Problem while scanning for variables in script directory.

--- ShowHexColorFromCursor.lua
+++ ShowHexColorFromCursor.lua
@@ -1,4 +1,80 @@
--- TIME_STAMP   2018-01-16 17:37:44   v 0.8
+-- TIME_STAMP   2018-02-20 18:18:22   v 0.9
+
+--[[------------- I N S T A L L A T I O N   A N D   U S I N G   I N S T R U C T I O N --------------
+Save the file.
+At first, make an entry in your SciTEStartup.lua
+
+LoadLuaFile("ShowHexColorFromCursor.lua", "C:\\Your Path\\with Backslash\\")
+
+
+Select free command-numbers from your SciTEUser.properties.
+Customize the following settings with this numbers.
+
+
+# 13 Show HexColor RGB
+command.name.13.*=Show RGB-Color From Cursor
+command.13.*=dostring ShowHexColorFromCursor()
+command.mode.13.*=subsystem:lua,savebefore:yes
+command.shortcut.13.*=Ctrl+Shift+F11
+
+# 14 Show HexColor BGR
+command.name.14.*=Show BGR-Color From Cursor
+command.14.*=dostring ShowHexColorFromCursor(true)
+command.mode.14.*=subsystem:lua,savebefore:yes
+command.shortcut.14.*=Ctrl+Alt+F11
+
+
+Set the cursor in the Hex-value, press the hotkey to show the color as RGB or as BGR.
+Above the value a Call tip appears. The background color corresponds to the hex value.
+A possible alpha component is ignored.
+
+[NEW]
+Now be also recognized in au3 scripts, variables/constants which have an color assignment
+inside the script or inside an include file from this script.
+But it can only be one assignment per line.
+If the assignment is inside a comment line or -block, it will ignored.
+The assignment can also be build by using function(s) [from script or include files].
+example:  "Local $COLOR = '0x' & Hex(Mod(@SEC, 2) ? Random(0,0x000FFF, 1) : Random(0x001000, 0xFFF000, 1), 8)"
+But the functions must NOT CONTAIN any VARIABLES! This would require a recursive assignment search.
+Impossible if variables get values only at runtime.
+You can disable the search inside include files with an entry in SciTEUser.properties:
+#~ "ShowHexColorFromCursor.lua", Dis/Enable search in Includes (0/1  NO/YES)
+Get.Color.Assignment.Includes=0
+The default value (without settings) is '1', enabled.
+Includes in comments will ignored.
+
+Be recognized AutoIt hex color code "0x12AB34" and also HTML hex color code "#12AB34".
+
+
+
+PREVIEW FOR BACK AND FORE COLOR:
+- Write in one line first the back color, than the fore color (i. e. as comment: "; 0xDEDEDE 0x000080") OR
+  have this values inside a function call: "_AnyFunction($param1, $param2, 0xDEDEDE, $param3 0x000080)".
+  If the order inside the call is reverse (first hex value is fore color), you can call the function with Flag "_fFore1st=true"
+- No other color value may be included in this line. If any - the first and second color will used.
+- Set the cursor in this line and hit the Hotkey.
+- A Calltip appears with the back color and the text "FORE-COLOR" with color of the fore value.
+- If only one color value was find in this line, this value will used as back color or, if Flag is "true", as fore color.
+  In this cases the fore color is set to black and with Flag the back color is the default GUI back color "0xF0F0F0"
+
+For use with AutoIt color values only.
+
+To have both calls (w/wo flag) make two commands:
+
+# 11 Preview Back and Fore Color / first color value is back color
+command.name.11.*.au3=Preview Back and Fore Color
+command.11.*.au3=dostring PreviewBackForeColor()
+command.mode.11.*.au3=subsystem:lua,savebefore:yes
+command.shortcut.11.*.au3=Ctrl+Shift+F12
+
+# 16 Preview Fore and Back Color / first color value is fore color
+command.name.16.*.au3=Preview Fore and Back Color
+command.16.*.au3=dostring PreviewBackForeColor(true)
+command.mode.16.*.au3=subsystem:lua,savebefore:yes
+command.shortcut.16.*.au3=Ctrl+Alt+F12
+--------------------------------------------------------------------------------------------------]]
+
+local bDEBUG = false    -- set "true" to get debug output
 
 ------------------------------------------------------------ list object to manipulate simple tables
 local objList = {
@@ -44,6 +120,7 @@ local objColor = {
 	---------------------------------------------------------------------------------------- pattern
 	pattHex = '()0x([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])',
 	pattHex2 = '[0-9a-fA-F][0-9a-fA-F]',
+	pattHexEnd = '0x[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]()',
 	pattCS1 = '^#[Cc][Oo][Mm][Mm][Ee][Nn][Tt][Ss]%-[Ss][Tt][Aa][Rr][Tt]',
 	pattCE1 = '^#[Cc][Oo][Mm][Mm][Ee][Nn][Tt][Ss]%-[Ee][Nn][Dd]',
 	pattCS2 = '^#[Cc][Ss]',
@@ -58,10 +135,11 @@ local objColor = {
 
 	------------------------------------------------------------ set calltip values back to defaults
 	SetCalltipsDefault = function(self)
+		self.colortip_show = false
 		scite.SendEditor(SCI_CALLTIPSETBACK, 0xFFFFFF)
 		scite.SendEditor(SCI_CALLTIPSETFOREHLT, self.calltips_colorhlt_def)
 		scite.SendEditor(SCI_CALLTIPSETPOSITION, self.calltips_pos_def)
-		self.colortip_show = false
+		if bDEBUG then output:AppendText('> DEBUG: Calltips set to defaults') end
 	end,
 	---------------------------------------------------------------------------- /SetCalltipsDefault
 
@@ -113,7 +191,10 @@ local objColor = {
 				-- #include <abc.au3>
 				incl = line:match("#[iI][nN][cC][lL][uU][dD][eE]%s-<([%w%s_.]+)>")
 				if incl ~= nil then
-					if self.lIncl:addonce(incl) then self.sIncl = self.sIncl..'#include <'..incl..'>\n' end
+					if self.lIncl:addonce(incl) then
+						self.sIncl = self.sIncl..'#include <'..incl..'>\n'
+						if bDEBUG then output:AppendText('> DEBUG: IncludesFromBuffer.Add "#include <'..incl..'>"\n') end
+					end
 				else
 					-- #include 'abc.au3' or #include "abc.au3"
 					_, incl = line:match("#[iI][nN][cC][lL][uU][dD][eE]%s-([\"'])([%w%s_.:\\]+)%1")
@@ -122,8 +203,10 @@ local objColor = {
 						if self.lIncl:addonce(incl) then
 							if incl:sub(2,2) == ':' then
 								self.sIncl = self.sIncl..'#include "'..incl..'"\n'
+								if bDEBUG then output:AppendText('> DEBUG: IncludesFromBuffer.Add "#include '.."'"..incl.."'"..'"\n') end
 							else
 								self.sIncl = self.sIncl..'#include "'..props['FileDir']..'\\'..incl..'"\n'
+								if bDEBUG then output:AppendText('> DEBUG: IncludesFromBuffer.Add "#include '.."'"..props['FileDir']..'\\'..incl.."'"..'"\n') end
 							end
 						end
 					end
@@ -161,6 +244,7 @@ local objColor = {
 				if boolCmnt then countCmnt = countCmnt + (n) end
 				if not boolCmnt and countCmnt == 0 then     -- none comment line or block
 					if line:find(_sSelection..'%s*=') then
+						if bDEBUG then output:AppendText('> DEBUG: Assignment line  "'..line..'"\n') end
 						return line
 					end
 				end
@@ -182,12 +266,21 @@ local objColor = {
 					for i=1, #self.lIncl.list do
 						if self.lIncl.list[i]:sub(2,2) == ':' then    -- include has full path, search only in this file
 							sLine = self:FindAssignment(self.lIncl.list[i], _sSelection)
-						elseif self.lIncl.list[i]:find('\\') then     -- include has partial path, check first if exist in @ScriptDir
+							if bDEBUG then output:AppendText('> DEBUG: Search "'.._sSelection..'" in "'..self.lIncl.list[i]..'"  --> '..tostring(sLine ~= nil)..'\n') end
+						end
+						if sLine == nil and self.lIncl.list[i]:find('\\') then     -- include has partial path, check first if exist in @ScriptDir
 							sLine = self:FindAssignment(props['FileDir']..'\\'..self.lIncl.list[i], _sSelection)
-						else                                          -- include has filename only (or partial path),
-							if sLine ~= nil then break end            -- .. concanate all directories with this for searching
+							if bDEBUG then output:AppendText('> DEBUG: Search "'.._sSelection..'" in "'..props['FileDir']..'\\'..self.lIncl.list[i]..'"  --> '..tostring(sLine ~= nil)..'\n') end
+						end
+						if sLine == nil then                          -- include has filename only (or partial path),
+                                                                      -- .. concanate all directories with this for searching
 							for j=1, #self.lInclPathes.list do
 								sLine = self:FindAssignment(self.lInclPathes.list[j]..'\\'..self.lIncl.list[i], _sSelection)
+								if bDEBUG then output:AppendText('> DEBUG: Search "'.._sSelection..'" in "'..self.lInclPathes.list[j]..'\\'..self.lIncl.list[i]..'"  --> '..tostring(sLine ~= nil)..'\n') end
+								if sLine == nil then
+									sLine = self:FindAssignment(props['FileDir']..'\\'..self.lIncl.list[i], _sSelection)
+									if bDEBUG then output:AppendText('> DEBUG: Search "'.._sSelection..'" in "'..props['FileDir']..'\\'..self.lIncl.list[i]..'"  --> '..tostring(sLine ~= nil)..'\n') end
+								end
 								if sLine ~= nil then break end
 							end
 						end
@@ -196,7 +289,12 @@ local objColor = {
 			end
 		end
 
-		if sLine == nil then sLine = 'NONE' end
+		if sLine == nil then
+			sLine = 'NONE'
+		else   -- trim characters right from assignment
+			sLine = sLine:sub(1,sLine:match(self.pattHexEnd))
+		end
+		if bDEBUG then output:AppendText('> DEBUG: Search "'.._sSelection..'" \n> DEBUG: Result "'..sLine..'"\n') end
 		-- create the au3-file for executing the assignment line, with 'NONE' - the last result file will delete
 		self:CreateAU3(sLine)
 		-- run the au3-file
@@ -215,6 +313,7 @@ local objColor = {
 			scite.SendEditor(SCI_CALLTIPSETBACK, 0x33FFFF)
 			scite.SendEditor(SCI_CALLTIPSETFOREHLT, 0x0000FF)
 			scite.SendEditor(SCI_CALLTIPSETPOSITION, true)
+			if bDEBUG then output:AppendText('> DEBUG: Set Calltip "'.._sSelection..'" --> "NONE COLOR ASSIGNED!"\n') end
 		else
 			local sValue = fH:read()
 			fH:close()
@@ -225,8 +324,10 @@ local objColor = {
 			scite.SendEditor(SCI_CALLTIPSETPOSITION, true)
 			if _fBGR == true then
 				scite.SendEditor(SCI_CALLTIPSETBACK, tonumber(string.format('0x%s%s%s', R,G,B)))
+				if bDEBUG then output:AppendText('> DEBUG: Set Calltip BGR  "'.._sSelection..'" --> "'..string.format('0x%s%s%s', R,G,B)..'"\n') end
 			else
 				scite.SendEditor(SCI_CALLTIPSETBACK, tonumber(string.format('0x%s%s%s', B,G,R)))
+				if bDEBUG then output:AppendText('> DEBUG: Set Calltip RGB  "'.._sSelection..'" --> "'..string.format('0x%s%s%s', B,G,R)..'"\n') end
 			end
 		end
 		self.colortip_show = true
@@ -261,6 +362,7 @@ local objColor = {
 			-- trim spaces on right site, if any
 			sSelection = sSelection:gsub('%s+$','')
 			var_endPos = var_endPos - (iLenSel - sSelection:len())
+			if bDEBUG then output:AppendText('> DEBUG: Cursor on variable "'..sSelection..'"\n') end
 			return self:GetColorValueFromVariable(sSelection, cursor, var_beginPos, var_endPos, _fBGR)
 		end
 
@@ -279,13 +381,16 @@ local objColor = {
 				return
 			end
 			local R,G,B = tostring(editor:GetSelText()):match('('..self.pattHex2..')('..self.pattHex2..')('..self.pattHex2..')$')
+			if bDEBUG then output:AppendText('> DEBUG: Cursor on hex value\n') end
 			scite.SendEditor(SCI_CALLTIPSHOW, beginPos+1, (' '):rep(iLen-1))
 			scite.SendEditor(SCI_CALLTIPSETHLT, 0, iLen-1)
 			scite.SendEditor(SCI_CALLTIPSETPOSITION, true)
 			if _fBGR == true then
 				scite.SendEditor(SCI_CALLTIPSETBACK, tonumber(string.format('0x%s%s%s', R,G,B)))
+				if bDEBUG then output:AppendText('> DEBUG: Set Calltip BGR hex value --> "'..string.format('0x%s%s%s', R,G,B)..'"\n') end
 			else
 				scite.SendEditor(SCI_CALLTIPSETBACK, tonumber(string.format('0x%s%s%s', B,G,R)))
+				if bDEBUG then output:AppendText('> DEBUG: Set Calltip RGB hex value --> "'..string.format('0x%s%s%s', B,G,R)..'"\n') end
 			end
 			self.colortip_show = true
 		end
@@ -305,22 +410,30 @@ local objColor = {
 			table.insert(tMatch, t)
 		end
 		if #tMatch == 0 then
+			if bDEBUG then output:AppendText('> DEBUG: Search back/fore color --> "FAILED"\n') end
 			return
 		elseif #tMatch == 1 then
 			if _fFore1st == true then
 				iForeCol = tonumber(string.format('0x%s%s%s', tMatch[1].B, tMatch[1].G, tMatch[1].R))
+				if bDEBUG then output:AppendText('> DEBUG: Search back/fore color "ForeColor" --> "'..string.format('0x%s%s%s', tMatch[1].B, tMatch[1].G, tMatch[1].R)..'"\n') end
 			else
 				iBackCol = tonumber(string.format('0x%s%s%s', tMatch[1].B, tMatch[1].G, tMatch[1].R))
+				if bDEBUG then output:AppendText('> DEBUG: Search back/fore color "BackColor" --> "'..string.format('0x%s%s%s', tMatch[1].B, tMatch[1].G, tMatch[1].R)..'"\n') end
 			end
 		else
 			if _fFore1st == true then
 				iForeCol = tonumber(string.format('0x%s%s%s', tMatch[1].B, tMatch[1].G, tMatch[1].R))
 				iBackCol = tonumber(string.format('0x%s%s%s', tMatch[2].B, tMatch[2].G, tMatch[2].R))
+				if bDEBUG then output:AppendText('> DEBUG: Search back/fore color "ForeColor" --> "'..string.format('0x%s%s%s', tMatch[1].B, tMatch[1].G, tMatch[1].R)..'"\n') end
+				if bDEBUG then output:AppendText('> DEBUG: Search back/fore color "BackColor" --> "'..string.format('0x%s%s%s', tMatch[2].B, tMatch[2].G, tMatch[2].R)..'"\n') end
 			else
 				iForeCol = tonumber(string.format('0x%s%s%s', tMatch[2].B, tMatch[2].G, tMatch[2].R))
 				iBackCol = tonumber(string.format('0x%s%s%s', tMatch[1].B, tMatch[1].G, tMatch[1].R))
+				if bDEBUG then output:AppendText('> DEBUG: Search back/fore color "ForeColor" --> "'..string.format('0x%s%s%s', tMatch[2].B, tMatch[2].G, tMatch[2].R)..'"\n') end
+				if bDEBUG then output:AppendText('> DEBUG: Search back/fore color "BackColor" --> "'..string.format('0x%s%s%s', tMatch[1].B, tMatch[1].G, tMatch[1].R)..'"\n') end
 			end
 		end
+		if bDEBUG then output:AppendText('> DEBUG: Set calltip back/fore color\n') end
 		scite.SendEditor(SCI_CALLTIPSHOW, iLineStartPos + beginPos, ' FORE-COLOR ')
 		scite.SendEditor(SCI_CALLTIPSETHLT, 0, 12)
 		scite.SendEditor(SCI_CALLTIPSETBACK, iBackCol)
@@ -335,11 +448,11 @@ local objColor = {
 ---------------------------------------------------------------------------------- region EventClass
 ShowColorEvt = EventClass:new(Common)
 
-function ShowColorEvt:OnKey(_keycode, _shift, _ctrl, _alt)
+function ShowColorEvt:OnKey()
 	if objColor.colortip_show then objColor:SetCalltipsDefault() end
 end
 
-function ShowColorEvt:OnDwellStart(_pos, _word)
+function ShowColorEvt:OnDwellStart()
 	if objColor.colortip_show then objColor:SetCalltipsDefault() end
 end
 --------------------------------------------------------------------------------- /region EventClass
@@ -360,5 +473,3 @@ end  --> PreviewBackForeColor
 ---------------------------------------------------------------------------------------- run startup
 objColor:Startup()
 ----------------------------------------------------------------------------------------------------
-
-
